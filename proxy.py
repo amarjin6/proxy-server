ebef6f51929b38f48d71b32cd5b2d66cae7ef453
Alexander Marjin <amarjin6@gmail.com>
2022-05-02 18:54:19 +0300

    Create server based on sockets

--- proxy.py
+++ proxy.py
@@ -1,26 +1,114 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import signal  # Module provides mechanisms for using signal handlers
+import socket
+import sys
+import threading
 
-HOST = '192.168.56.1'
-PORT = 8000
-
-
-class Proxy(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-        self.wfile.write(bytes('<html><body><h1>Hi!</h1></body></html>', 'utf-8'))
-
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-        self.wfile.write(bytes('<html><body><h1>Hi!</h1></body></html>', 'utf-8'))
+config = {
+    'HOST': '192.168.56.1',
+    'PORT': 8000,
+    'MAX_LEN': 1024,
+    'TIMEOUT': 5,
+    'MEMBERS_AMOUNT': 10
+}
 
 
-server = HTTPServer((HOST, PORT), Proxy)
-print('Server is running...')
-server.serve_forever()
-server.server_close()
+class Proxy:
+    def __init__(self, config: dict):
+        # Shutdown on Ctrl+C
+        signal.signal(signal.SIGINT, self.shutdown)
+
+        # Create a TCP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # Bind the socket to a public host, and a port
+        self.sock.bind((config['HOST_NAME'], config['BIND_PORT']))
+
+        self.sock.listen(config['MEMBERS_AMOUNT'])  # become a server socket
+        self.__clients = {}
+
+    def listen(self):
+        while True:
+            # Establish the connection
+            (conn, addr) = self.sock.accept()
+            d = threading.Thread(name=addr[0], target=self.redirect,
+                                 args=(conn, addr), daemon=True)
+            d.start()
+
+    @staticmethod
+    def redirect(conn, addr):
+        # Get the request from browser
+        request = conn.recv(config['MAX_REQUEST_LEN'])
+
+        # Parse the first line
+        firstLine = request.split('\n')[0]
+
+        # Get url
+        try:
+            url = firstLine.split(' ')[1]
+        except IndexError:
+            url = ''
+
+        httpPos = url.find("://")  # find pos of ://
+        if httpPos == -1:
+            temp = url
+        else:
+            temp = url[(httpPos + 3):]  # get the rest of url
+
+        portPos = temp.find(":")  # find the port pos (if any)
+
+        # Find end of web server
+        webserverPos = temp.find("/")
+        if webserverPos == -1:
+            webserverPos = len(temp)
+
+        webserver = ''
+        port = -1
+        if portPos == -1 or webserverPos < portPos:
+            # default port
+            port = 80
+            webserver = temp[:webserverPos]
+
+        else:
+            # specific port
+            port = int((temp[(portPos + 1):])[:webserverPos - portPos - 1])
+            webserver = temp[:portPos]
+
+        # Set up a new connection to the destination server
+        try:
+            # create a socket to connect to the web server
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(config['TIMEOUT'])
+            s.connect((webserver, port))
+            s.sendall(request)
+
+            # Redirect the server’s response to the client
+            while 1:
+                # receive data from web server
+                data = s.recv(config['MAX_LEN'])
+
+                if len(data) > 0:
+                    conn.send(data)  # send to browser/client
+                else:
+                    break
+
+                s.close()
+                conn.close()
+
+        except socket.error as e:
+            print(f'Failed to set up new connection: {addr} , {e}')
+            if s:
+                s.close()
+            if conn:
+                conn.close()
+
+    def shutdown(self):
+        """ Handle the exiting server. Clean all traces """
+        self.sock.close()
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    print('Server is running...')
+    server = Proxy(config)
+    server.listen()
